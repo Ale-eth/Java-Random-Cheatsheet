a81d8ec41d0e6b7da1da29e97935b7ba91342cf8
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-05-09 16:39:04 -0300

    Update Java-Random-Cheatsheet.java

--- Java-Random-Cheatsheet.java
+++ Java-Random-Cheatsheet.java
@@ -1,1 +1,15 @@
+import java.util.Random;        // La libreria Random permite generar numeros aleatorios int y float
 
+
+Random random = new Random();       // Se instancia un Random primero
+
+int min = 1;        // Establecemos un minimo y un maximo para generar el random
+int max = 10;
+
+
+int random1 = random.nextInt(max + min) + min;  // Primera forma
+
+int random2 = (int) (Math.random()*(max-min)) + min;    // Segunda forma
+
+System.out.println(random1);
+System.out.println(random2);
